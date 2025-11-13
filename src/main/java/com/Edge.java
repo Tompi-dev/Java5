56e5ad7a410eadb44d54cd4710dd6702b0422b4a
Tompi-dev <tomiris.kassymova.98@gmail.com>
2025-11-13 20:32:50 +0500

    Add Edge and Union-Find (DSU) data structures

--- src/main/java/com/Edge.java
+++ src/main/java/com/Edge.java
@@ -1,4 +1,21 @@
 package com;
 
-public class Edge {
+public class Edge implements Comparable<Edge> {
+    public int u, v, w;
+
+    public Edge(int u, int v, int w) {
+        this.u = u;
+        this.v = v;
+        this.w = w;
+    }
+
+    @Override
+    public int compareTo(Edge other) {
+        return this.w - other.w;
+    }
+
+    @Override
+    public String toString() {
+        return u + " - " + v + " (" + w + ")";
+    }
 }
--- src/main/java/com/UF.java
+++ src/main/java/com/UF.java
@@ -1,4 +1,21 @@
 package com;
 
 public class UF {
+    private int[] parent;
+
+    public UF(int n) {
+        parent = new int[n];
+        for (int i = 0; i < n; i++)
+            parent[i] = i;
+    }
+
+    public int find(int x) {
+        if (parent[x] != x)
+            parent[x] = find(parent[x]);
+        return parent[x];
+    }
+
+    public void union(int a, int b) {
+        parent[find(a)] = find(b);
+    }
 }
